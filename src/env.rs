72e96cc683d24b72709c6e505f1e4b86d0d3b78a
蔡蔡 <caigq@192.168.0.7>
2024-10-19 19:57:20 +0800

    refactor: separate the logic regarding env

---
+++ src/env.rs
@@ -0,0 +1,91 @@
+use crate::{ENV_NOT_VALID_FIELDS, ENV_VALID_FIELDS};
+use anyhow::Result;
+use dotenv;
+use std::path::PathBuf;
+
+#[allow(dead_code)]
+pub fn load_env(env_path: &PathBuf) -> Result<()> {
+    eprintln!("加载环境变量中");
+    // 读取环境变量文件
+    dotenv::from_path(env_path)?;
+    eprintln!("加载环境变量成功");
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn valid_env() -> Result<()> {
+    for field in ENV_VALID_FIELDS {
+        match dotenv::var(field) {
+            Ok(value) => {
+                if value.trim().is_empty() {
+                    return Err(anyhow::anyhow!("字段 {} 不能为空 ", field));
+                } else {
+                    if field == "SEND_EMAIL" && value == "1" {
+                        for field in ENV_NOT_VALID_FIELDS {
+                            match dotenv::var(field) {
+                                Ok(value) => {
+                                    if value.trim().is_empty() {
+                                        return Err(anyhow::anyhow!("字段 {} 不能为空 ", field));
+                                    }
+                                }
+                                Err(_) => {
+                                    return Err(anyhow::anyhow!("请添加 {} 字段 ", field));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Err(_) => {
+                return Err(anyhow::anyhow!("请添加 {} 字段 ", field));
+            }
+        }
+    }
+    Ok(())
+}
+
+#[allow(dead_code)]
+pub fn env_file_exist(env_path: &PathBuf) -> bool {
+    // 判断环境变量文件是否存在
+    if !env_path.exists() {
+        // println!("以下字段为必须： ");
+        // for field in ENV_VALID_FIELDS {
+        //     println!("{}", field);
+        // }
+        // println!("以下字段非必须： （如果SEND_EMAIL为1, 则以下字段为必须）");
+        // for field in ENV_NOT_VALID_FIELDS {
+        //     println!("{}", field);
+        // }
+        return false;
+    }
+    true
+}
+
+// 测试模块
+#[cfg(test)]
+pub mod tests {
+    use super::{env_file_exist, load_env};
+    use std::path::PathBuf;
+    #[test]
+    fn test_env_file_not_exist() {
+        let env_path = dirs::home_dir().unwrap().join(".env");
+        assert_eq!(env_file_exist(&env_path), false);
+    }
+    #[test]
+    fn test_env_file_exist() {
+        let env_path = PathBuf::new().join(".env");
+        assert_eq!(env_file_exist(&env_path), true);
+    }
+    #[test]
+    fn test_load_env_err() {
+        let env_path = dirs::home_dir().unwrap().join(".env");
+        let res = load_env(&env_path);
+        assert_eq!(res.is_ok(), false);
+    }
+    #[test]
+    fn test_load_env_ok() {
+        let env_path = PathBuf::new().join(".env");
+        let res = load_env(&env_path);
+        assert_eq!(res.is_ok(), true);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+mod env;
+use anyhow::Result;
 use chrono_tz::Asia::Shanghai;
+use env::{env_file_exist, load_env, valid_env};
 use reqwest::{header, Client};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, path::PathBuf, process};
 use tokio::time::{sleep, Duration};
-use tokio_cron_scheduler::{Job, JobBuilder, JobScheduler};
+use tokio_cron_scheduler::{Job, JobScheduler};
 
-const ENV_VALID_FIELDS: [&str; 4] = ["COOKIE", "UUID", "AID", "SEND_EMAIL"];
-const ENV_NOT_VALID_FIELDS: [&str; 5] = [
+pub const ENV_VALID_FIELDS: [&str; 4] = ["COOKIE", "UUID", "AID", "SEND_EMAIL"];
+pub const ENV_NOT_VALID_FIELDS: [&str; 5] = [
     "SMTP_USER",
     "SMTP_PASS",
     "SMTP_SERVER",
@@ -31,19 +34,24 @@ struct SuccessData {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // 定义环境变量文件路径
     let env_path = dirs::home_dir().unwrap().join(".env");
-    // let env_path = PathBuf::new().join(".env");
+
     // 判断环境变量文件是否存在
     if !env_file_exist(&env_path) {
-        return;
+        return Err(anyhow::anyhow!(
+            "环境变量文件不存在,请在以下路径创建.env文件： {}",
+            env_path.display()
+        ));
     }
 
-    let _ = load_env(&env_path);
+    load_env(&env_path)?;
+
+    valid_env()?;
 
     // 初始化调度器
-    let schedule = JobScheduler::new().await.unwrap();
+    let schedule = JobScheduler::new().await?;
 
     // 定时任务1： 每天 早上八点 执行自动签到
     let check_in_job = Job::new_async_tz("* * 8 * * * ", Shanghai, |_uuid, _lock| {
@@ -61,8 +69,7 @@ async fn main() {
                 );
             }
         })
-    })
-    .unwrap();
+    })?;
     // 定时任务2： 每隔一个月发送邮件提醒更换session
     // let send_email_job = Job::new_async_tz("1/10 * * * * *", Shanghai,|_uuid, _lock| {
     //     Box::pin(async move {
@@ -75,93 +82,34 @@ async fn main() {
     // .unwrap();
 
     // 添加定时任务
-    schedule.add(check_in_job).await.unwrap();
+    schedule.add(check_in_job).await?;
     // schedule.add(send_email_job).await.unwrap();
 
     // 启动调度器
-    schedule.start().await.unwrap();
+    schedule.start().await?;
 
     // 保持程序运行
     loop {
         sleep(Duration::from_secs(60)).await;
     }
 }
-#[allow(dead_code)]
-fn env_file_exist(env_path: &PathBuf) -> bool {
-    let mut path = env_path.clone();
-    // 判断环境变量文件是否存在
-    if !path.exists() {
-        // 提示用户创建环境变量文件， 将需要的配置信息字段打印出来
-        path.pop();
-        println!("请在以下路径创建.env文件： {}", path.display());
-        println!("以下字段为必须： ");
-        for field in ENV_VALID_FIELDS {
-            println!("{}", field);
-        }
-        println!("以下字段非必须： （如果SEND_EMAIL为1, 则以下字段为必须）");
-        for field in ENV_NOT_VALID_FIELDS {
-            println!("{}", field);
-        }
-        return false;
-    }
-    true
-}
 
 #[allow(dead_code)]
-fn load_env(env_path: &PathBuf) {
-    // 读取环境变量文件
-    let _ = dotenv::from_path(env_path);
-    eprintln!("加载环境变量中");
-    for field in ENV_VALID_FIELDS {
-        match dotenv::var(field) {
-            Ok(value) => {
-                if value.trim().is_empty() {
-                    eprintln!("字段 {} 不能为空 ", field);
-                    process::exit(1);
-                } else {
-                    if field == "SEND_EMAIL" && value == "1" {
-                        for field in ENV_NOT_VALID_FIELDS {
-                            match dotenv::var(field) {
-                                Ok(value) => {
-                                    if value.trim().is_empty() {
-                                        eprintln!("字段 {} 不能为空 ", field);
-                                        process::exit(1);
-                                    }
-                                }
-                                Err(_) => {
-                                    eprintln!("请添加 {} 字段 ", field);
-                                    process::exit(1);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            Err(_) => {
-                eprintln!("请添加 {} 字段 ", field);
-                process::exit(1);
-            }
-        }
-    }
-    eprintln!("加载环境变量成功");
-}
-
-#[allow(dead_code)]
-async fn auto_check_in() -> reqwest::Result<()> {
+async fn auto_check_in() -> Result<()> {
     let client = Client::new();
     // 自定义请求头
     let mut headers = header::HeaderMap::new();
 
-    let mut cookie = dotenv::var("COOKIE").unwrap();
+    let mut cookie = dotenv::var("COOKIE")?;
     cookie.insert_str(0, "sessionid=");
 
-    headers.insert(header::COOKIE, cookie.parse().unwrap());
-    headers.insert(header::REFERER, ROOT_URL.parse().unwrap());
+    headers.insert(header::COOKIE, cookie.parse()?);
+    headers.insert(header::REFERER, ROOT_URL.parse()?);
 
     // 自定义请求参数
     let mut params = HashMap::new();
-    params.insert("uuid", dotenv::var("UUID").unwrap());
-    params.insert("aid", dotenv::var("AID").unwrap());
+    params.insert("uuid", dotenv::var("UUID")?);
+    params.insert("aid", dotenv::var("AID")?);
     params.insert("spider", 0.to_string());
     let response = client
         .post(CHECK_IN_URL)
@@ -172,7 +120,7 @@ async fn auto_check_in() -> reqwest::Result<()> {
 
     // 打印响应状态码
     let status = response.status();
-    // 打印响应内容
+
     let body: ResponseData = response.json().await?;
     eprintln!("请求状态: {:#?}, 响应内容: {:?}", status, body);
 
@@ -180,6 +128,6 @@ async fn auto_check_in() -> reqwest::Result<()> {
 }
 
 #[allow(dead_code)]
-fn auto_send_email() -> reqwest::Result<()> {
+fn auto_send_email() -> Result<()> {
     Ok(())
 }
